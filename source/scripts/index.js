aa285f65118cc11804798182032c9632c4da292b
Мария Курильчик <139053449+MaremianaComp@users.noreply.github.com>
2025-05-21 18:26:08 +0200

    Adds index.js (#22)

---
+++ source/scripts/index.js
@@ -0,0 +1,23 @@
+/* КАРТА */
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  const ymaps = window.ymaps;
+  ymaps.ready(() => {
+    const myMap = new ymaps.Map('map', {
+        center: [59.938679, 30.3230044],
+        zoom: 14,
+        controls: []
+      }),
+
+      myPlacemark = new ymaps.Placemark(myMap.getCenter(), {}, {
+        iconLayout: 'default#image',
+        iconImageHref: 'images/map-pin.png',
+        iconImageSize: [57, 53],
+        iconImageOffset: [-26, -42]
+      });
+
+    myMap.geoObjects.add(myPlacemark);
+  });
+
+});
